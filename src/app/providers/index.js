92d484489db6b0276feb12b773c90917aaeb94b2
Prabhat <pchandra114@hotmail.com>
2026-07-22 02:59:09 +0530

    chore(core): configure path aliases

---
+++ src/app/providers/index.js
@@ -0,0 +1,3 @@
+export { default as AppThemeProvider } from "./AppThemeProvider";
+export { default as AppReduxProvider } from "./AppReduxProvider";
+export { default as AppQueryProvider } from "./AppQueryProvider";
